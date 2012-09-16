fbb4f6ad882a5ba9480186c6872c63c034addc3f
Zenlan Ltd <mszpak@zenlan.com>
2012-09-16 16:21:05 +0100

    Updated views titles, menus etc. Changed index status block declaration, split out one for each index

--- lambeth_opendata_documents_istat.tpl.php
+++ lambeth_opendata_documents_istat.tpl.php
@@ -4,26 +4,20 @@
  * Template file for lambeth_opendata documents table
  */
 ?>
-<div class="<?php echo $class; ?>">
-  <table>
-    <tr>
-      <?php
-      foreach ($indexes as $index) {
-        if ($index->enabled) {
-          $class = '#98C5AB';
-          $status = 'enabled';
-        }
-        else {
-          $class = '#CE7898';
-          $status = 'disabled';
-        }
-        ?>
-        <th style="background:<?php echo $class; ?>;align:center;">
-          <?php echo 'Index ' . $index->item_type . '<br /> is currently ' . $status; ?>
-        </th>
-        <?php
-      }
-      ?>
-    </tr>
-  </table>
-</div>
+<?php
+foreach ($indexes as $index) {
+  if ($index->enabled) {
+    $class = '#98C5AB';
+    $status = 'enabled';
+  }
+  else {
+    $class = '#CE7898';
+    $status = 'disabled';
+  }
+  ?>
+  <div style="background:<?php echo $class; ?>;text-align:center;">
+    <?php echo $index->name . ' is currently ' . $status; ?>
+  </div>
+  <?php
+}
+?>
